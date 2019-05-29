8cfda85399092769341c80453940a2231e40f6f5
janarthanana <jana12raz@gmail.com>
2019-05-30 00:41:05 +0530

    Initial Branch with Admin online

---
+++ application/admin/logs/log-2019-05-29.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-29 20:33:37 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\snm\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-05-29 20:33:50 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\snm\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-05-29 20:33:50 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\snm\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-05-29 20:34:04 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\snm\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2019-05-29 20:34:04 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\snm\system\database\drivers\mysql\mysql_driver.php 91
---
+++ application/controllers/main.php
@@ -0,0 +1,11 @@
+<?php 
+defined('BASEPATH') OR exit('No Direct Script Access allowed');
+
+
+class Main extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->view("login");
+	}
+}
